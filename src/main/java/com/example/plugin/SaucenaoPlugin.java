f1dc002c1da1c35586052035b2bf73d7a8d8027c
HorizonChaser <ahorizonchaser@gmail.com>
2021-04-17 15:23:19 +0800

    Implemented Quota Limit

--- src/main/java/com/example/plugin/SaucenaoPlugin.java
+++ src/main/java/com/example/plugin/SaucenaoPlugin.java
@@ -2,11 +2,11 @@ package com.example.plugin;
 
 import com.example.util.CommonDefinition;
 import com.example.util.FileUtil;
+import com.example.util.PluginSystemBase;
 import net.mamoe.mirai.console.extension.PluginComponentStorage;
-import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
 import net.mamoe.mirai.event.GlobalEventChannel;
-import net.mamoe.mirai.event.events.GroupMessageEvent;
+import net.mamoe.mirai.event.events.MessageEvent;
 import net.mamoe.mirai.message.data.*;
 import net.mamoe.mirai.utils.ExternalResource;
 import org.jetbrains.annotations.NotNull;
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class SaucenaoPlugin extends JavaPlugin {
+public class SaucenaoPlugin extends PluginSystemBase {
     public static SaucenaoPlugin INSTANCE = new SaucenaoPlugin();
     public static int shortLimit = -1, longLimit = -1;
 
@@ -46,7 +46,7 @@ public class SaucenaoPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class, (GroupMessageEvent event) -> {
+        GlobalEventChannel.INSTANCE.subscribeAlways(MessageEvent.class, (MessageEvent event) -> {
 
             //TODO: Quota support for all users
             //TODO: Blacklist
@@ -55,7 +55,7 @@ public class SaucenaoPlugin extends JavaPlugin {
             long sender = event.getSender().getId();
             System.out.println(content);
 
-            if (sender == 1783861062L && content.contains("出处")) {
+            if (content.contains("出处")) {
                 File statFile = new File(CommonDefinition.fileRootPath + CommonDefinition.statFileName);
                 try {
                     BufferedWriter statFileWriter = new BufferedWriter(new FileWriter(statFile));
@@ -65,18 +65,34 @@ public class SaucenaoPlugin extends JavaPlugin {
                     e.printStackTrace();
                 }
 
+                ResponseInfo responseInfo = super.request(sender);
+
+                if (responseInfo.rejectReason == RejectReason.LongLimitReached) {
+                    MessageChain messages = new At(sender).plus(" 你已经达到长上限啦, 稍等会儿再来\n长上限每小时刷新一次~");
+                    event.getSubject().sendMessage(messages);
+                    return;
+                }
+                if (responseInfo.rejectReason == RejectReason.ShortLimitReached) {
+                    MessageChain messages = new At(sender).plus(" 你已经达到短上限啦, 稍等会儿再来\n短上限每40秒刷新一次~");
+                    event.getSubject().sendMessage(messages);
+                    return;
+                }
+
                 MessageChain chain = event.getMessage();
                 Image image = chain.get(Image.Key);
                 String imageName = CommonDefinition.fileRootPath + FileUtil.getTimeStamp() + ".jpg";
                 if (image != null) {
                     try {
                         FileUtil.download(Image.queryUrl(image), imageName);
+                        event.getSubject().sendMessage(new At(sender).
+                                plus(" 收到啦\n你的短上限还剩" + responseInfo.remainShort
+                                        + "\n你的长上限还剩" + responseInfo.remainLong));
                     } catch (Exception e) {
-                        System.out.println(imageName);
+                        System.out.println("[Error] Failed to download " + imageName);
+                        event.getSubject().sendMessage("校园网又又又又出错了, 图没下下来...要不重发一下?");
                         e.printStackTrace();
                     }
                 }
-                event.getSubject().sendMessage("在找了在找了, 稍等");
 
                 //DBG
                 System.out.println("[New Req] " + sender + " " + imageName);
@@ -104,11 +120,12 @@ public class SaucenaoPlugin extends JavaPlugin {
                 }
 
                 if (returnVal == 403) {
-                    event.getGroup().sendMessage("⚠ 出了点儿小问题, 喵酱的 API Key 被服务器拒绝了, 快去找 Horizon");
+                    event.getSubject().sendMessage("⚠ 出了点儿小问题, 喵酱的 API Key 被服务器拒绝了, 快去找 Horizon");
                     return;
                 }
                 if (returnVal != 200 && returnVal != 0) {
-                    event.getGroup().sendMessage("图片没 Post 到服务器, 要喵酱说这锅得归校园网背🔨");
+                    System.out.println("[Error] Unhandled returnVal of Python: " + returnVal);
+                    event.getSubject().sendMessage("图片没 Post 到服务器, 要喵酱说这锅得归校园网背🔨");
                     return;
                 }
 
@@ -117,6 +134,14 @@ public class SaucenaoPlugin extends JavaPlugin {
                 double similarity = Double.parseDouble(output.get(2));
                 String database = output.get(3), memberName = null,
                         title = null, fileName = null, url = null, pid = null;
+
+                if (similarity < CommonDefinition.minSimilarity) {
+                    MessageChain messages = new PlainText("喵酱已经努力找了, 不过没有找到足够相似的")
+                            .plus(new Face(Face.KUAI_KU_LE).plus(new At(sender)));
+                    event.getSubject().sendMessage(messages);
+                    return;
+                }
+
                 if (database.equals("Pixiv")) {
                     memberName = output.get(4);
                     title = output.get(5);
@@ -127,15 +152,6 @@ public class SaucenaoPlugin extends JavaPlugin {
                     url = output.get(4);
                 }
 
-                if (similarity < CommonDefinition.minSimilarity) {
-                    MessageChain messages = new PlainText("喵酱已经努力找了, 不过没有找到足够相似的")
-                            .plus(new Face(Face.KUAI_KU_LE).plus(new At(sender)));
-                    event.getGroup().sendMessage(messages);
-                    //TEST
-                    System.out.println(messages.contentToString());
-                    return;
-                }
-
                 StringBuilder response = new StringBuilder();
                 response.append("在 ").append(database).append(" 中找到啦!\n");
                 response.append("相似度: ").append(similarity).append("%\n");
@@ -144,14 +160,14 @@ public class SaucenaoPlugin extends JavaPlugin {
                     response.append("标题: ").append(title).append("\n");
                     response.append("pid: ").append(pid);
                 }
+                event.getSubject().sendMessage(response.toString());
                 // TODO
                 // response.append("link: ").append(url);
+
                 Image upload = ExternalResource.uploadAsImage(
                         new File(CommonDefinition.fileRootPath + fileName), event.getSubject());
-
-                event.getGroup().sendMessage(response.toString());
                 MessageChain messages = new PlainText("").plus(upload);
-                event.getGroup().sendMessage(messages);
+                event.getSubject().sendMessage(messages);
             }
         });
     }
---
+++ src/main/java/com/example/util/PluginSystemBase.java
@@ -0,0 +1,58 @@
+package com.example.util;
+
+import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
+import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class PluginSystemBase extends JavaPlugin {
+    static int shortLimit = 1, longLimit = 10;
+    static long shortDuration = 40, longDuration = 60 * 60;
+    static Map<Long, List<Long>> usageHistory = new HashMap<>();
+
+    public enum RejectReason {
+        Accepted,
+        ShortLimitReached,
+        LongLimitReached;
+    }
+
+    public PluginSystemBase(@NotNull JvmPluginDescription description) {
+        super(description);
+    }
+
+    public class ResponseInfo {
+        public RejectReason rejectReason;
+        public int remainShort, remainLong;
+
+        ResponseInfo(RejectReason rejectReason, int remainShort, int remainLong) {
+            this.rejectReason = rejectReason;
+            this.remainShort = remainShort;
+            this.remainLong = remainLong;
+        }
+    }
+
+    public ResponseInfo request(long user) {
+        if (!usageHistory.containsKey(user)) {
+            usageHistory.put(user, new ArrayList<>());
+            usageHistory.get(user).add(new Date().getTime()/1000);
+            return new ResponseInfo(RejectReason.Accepted, 0, longLimit - 1);
+        }
+
+        long now = new Date().getTime()/1000;
+        List<Long> userHistory = usageHistory.get(user);
+        int shortR = (now - userHistory.get(userHistory.size() - 1) > shortDuration) ? 1 : 0;
+
+        userHistory.removeIf(time -> now - time > longDuration);
+
+        if (userHistory.size() >= longLimit) {
+            return new ResponseInfo(RejectReason.LongLimitReached, shortR, 0);
+        }
+        if (shortR == 0) {
+            return new ResponseInfo(RejectReason.ShortLimitReached, 0, longLimit - userHistory.size());
+        }
+
+        userHistory.add(now);
+        return new ResponseInfo(RejectReason.Accepted, 0, longLimit - userHistory.size());
+    }
+}
